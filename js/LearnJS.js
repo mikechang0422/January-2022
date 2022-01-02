8bccc5ed2790feda76009a7e514a953e20c1420d
mike <mikechang0422@gmail.com>
2022-01-03 00:39:31 +0800

    設定明年倒數計時，月份跟日期另外設計

--- js/LearnJS.js
+++ js/LearnJS.js
@@ -43,3 +43,56 @@ document.querySelector('#mathPiParseInt').textContent = parseInt(Math.PI)
 let piFix = Math.PI
 piFix = piFix.toFixed(2)
 document.querySelector('#mathPiParseFloat').textContent = parseFloat(piFix)
+
+//set time out
+
+let countDown = function(due) {
+    let now = new Date()
+    let res = due.getTime() - now.getTime()
+    let sec = Math.floor(res /1000 %60)
+    let min = Math.floor(res /1000 /60) %60
+    let hour = Math.floor(res /1000 /60 /60) %24
+    let day = Math.floor(res /1000 /60 /60 /24)
+    let count = [day,hour,min,sec]
+    return count
+}
+
+
+let counter = function(){
+    let goal = new Date()
+    goal.setHours(24)
+    goal.setMinutes(0)
+    goal.setSeconds(0)
+
+    document.querySelector('#setTime').textContent = `${countDown(goal)[1]}小時${countDown(goal)[2]}分${countDown(goal)[3]}秒`
+}
+
+setInterval(()=>{
+    counter()
+},1000)
+
+let getNewYear = function(){
+    let newYear = new Date(2024,1,1)
+    let now = new Date()
+
+    let timeold = newYear.getTime() - now.getTime()
+    let sec = Math.floor(timeold /1000 %60)
+    let min = Math.floor(timeold /1000 /60) %60
+    let hour = Math.floor(timeold /1000 /60 /60) %24
+    // let lostDay = [31,28,31,30,31,30,31,31,30,31,30,31] //配合lostDay[mon]反向讀取日期總數
+    let lostDay = [31,30,31,30,31,31,30,31,30,31,28,31]
+    let nowDay = now.getDate()
+    let mon = 12 - newYear.getMonth()
+    day = lostDay[mon] - nowDay
+    let countNewYear = [mon,day,hour,min,sec]
+    return countNewYear
+}
+
+function setNewYear(){
+    getNewYear()
+    console.log(getNewYear())
+    document.querySelector('#setTime2').textContent = `${getNewYear(0)[0]}月${getNewYear(0)[1]}日${getNewYear(0)[2]}小時${getNewYear(0)[3]}分${getNewYear(0)[4]}秒`
+}
+setInterval(()=>{
+    setNewYear()
+},1000)
